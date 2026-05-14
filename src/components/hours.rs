e8d1273632e413cc44ad5ebcd03196c433d02f39
orkeilius <63045634+orkeilius@users.noreply.github.com>
2026-05-14 18:07:49 +0200

    tech[]: add basic test

--- src/components/hours.rs
+++ src/components/hours.rs
@@ -32,3 +32,32 @@ fn time_to_next_minute() -> std::time::Duration {
     let seconds_to_next_minute = 60 - Local::now().second();
     std::time::Duration::from_secs(seconds_to_next_minute as u64)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn renders_current_hour() {
+        let pre_hour = Local::now();
+        let result = dioxus_ssr::render_element(rsx! { Hours {} });
+        let post_hour = Local::now(); // add another check when test is run while minute change
+
+        assert!(
+            result.contains(&pre_hour.format("%H:%M").to_string())
+                || result.contains(&post_hour.format("%H:%M").to_string())
+        );
+    }
+
+    #[test]
+    fn test_time_to_next_minute() {
+        let seconds_before = Local::now().second();
+        let result = time_to_next_minute();
+
+        // Account for test execution time
+        let expected_min = (60 - seconds_before - 1) as u64;
+        let expected_max = (60 - seconds_before + 1) as u64;
+
+        assert!(result.as_secs() >= expected_min && result.as_secs() <= expected_max,);
+    }
+}
