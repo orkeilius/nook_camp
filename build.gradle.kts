f9e0c1f3c302739f9b680889c57996ceff952c14
orkeilius <63045634+orkeilius@users.noreply.github.com>
2026-05-17 10:25:07 +0200

    add sonar scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,12 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    id("org.sonarqube") version "7.3.0.8198"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "orkeilius_nook_camp")
+        property("sonar.organization", "orkeilius")
+    }
 }
